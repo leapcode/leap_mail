e7945706608f5a75fe7baf9a3cb268ddb49773bc
Kali Kaneko <kali@leap.se>
2013-11-15 23:28:02 -0200

    improve deb_release script

--- deb_release.sh
+++ deb_release.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 
-rm src/leap/mail/_version.py
+VERSION_FILE="src/leap/mail/_version.py"
+rm ${VERSION_FILE}
 python setup.py freeze_debianver
-sed -i 's/-dirty//g' src/leap/mail/_version.py 
-git add src/leap/mail/_version.py
+sed -i 's/-dirty//g' ${VERSION_FILE}
+git add ${VERSION_FILE}
 git ci -m "freeze debian version"	
